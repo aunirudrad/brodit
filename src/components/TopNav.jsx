7a2336270e4e57a0b54ec67ed9002143c99a675a
aunirudrad <anu15-4170@diu.edu.bd>
2026-03-05 03:13:39 +0600

    initial Commit

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BiPhoneCall, BiUser } from 'react-icons/bi';
+import { PiPencil } from 'react-icons/pi';
+
+const TopNav = () => {
+    return (
+        <div className='flex justify-between items-center py-8'>
+            <div className='flex gap-8'>
+                <div className='flex gap-6 items-center justify-between'>
+                    <BiPhoneCall></BiPhoneCall>
+                    <p>013-1110-7482</p>
+                </div>
+                <div className='flex gap-6 items-center justify-between'>
+                    <PiPencil></PiPencil>
+                    <p>BRODIT@EXAMPLE.COM</p>
+                </div>
+            </div>
+            <div className='flex gap-6 items-center justify-between'>
+                <BiUser></BiUser>
+                <button className='font-mono'>LOG IN / SIGN UP</button>
+            </div>
+
+        </div>
+    );
+};
+
+export default TopNav;
---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import TopNav from '../components/TopNav';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            {/* header */}
+            <header className='max-w-[90%] mx-auto'>
+                <TopNav></TopNav>
+
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    );
+};
+
+export default HomeLayout;
